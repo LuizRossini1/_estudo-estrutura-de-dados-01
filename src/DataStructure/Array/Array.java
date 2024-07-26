1ad99e455e23c2f0d3635fa977ccecdf21d0c771
LuizRossini <luiz.guirossini123@gmail.com>
2024-07-26 20:53:22 -0300

    overrinding toSting and new method

--- src/DataStructure/Array/Array.java
+++ src/DataStructure/Array/Array.java
@@ -1,19 +1,36 @@
 package DataStructure.Array;
 
+import java.util.Arrays;
+
 public class Array {
     //defining array
     private Car[] cars = new Car[10];
 
+    //variables
+    private int totalList;
+
+    //getter
+    public int getTotalList() {
+        return totalList;
+    }
+
     //adding index
     public void adding(Car car) {
+        this.cars[totalList] = car;
+        totalList++;
     }
 
     //removing index
-    public void remove(Car car) {
+    public void remove(int index) {
     }
 
     //show indexes
-    public void show() {
-        System.out.println(cars.length);
+    public int show() {
+        return getTotalList();
+    }
+
+    @Override
+    public String toString() {
+        return Arrays.toString(cars);
     }
 }
--- src/DataStructure/Array/ArrayTest.java
+++ src/DataStructure/Array/ArrayTest.java
@@ -1,14 +1,16 @@
 package DataStructure.Array;
 
-public class Main {
+public class ArrayTest {
     public static void main(String[] args) {
         var car1 = new Car("BMW");
         var car2 = new Car("Ferrai");
 
         var list = new Array();
 
-       list.adding(car1);
-       list.adding(car2);
-       list.show();
+        list.adding(car1);
+        list.adding(car2);
+
+        System.out.println("Total indexes: " +list.show());
+        System.out.println("Indexes: " +list);
     }
 }
