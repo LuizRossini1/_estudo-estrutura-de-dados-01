93fb7386d733d41d4396b7194f0c7038cf3f65c7
LuizRossini <luiz.guirossini123@gmail.com>
2024-07-26 20:38:47 -0300

    implementing logic and creating test class

---
+++ src/DataStructure/Array/Array.java
@@ -0,0 +1,19 @@
+package DataStructure.Array;
+
+public class Array {
+    //defining array
+    private Car[] cars = new Car[10];
+
+    //adding index
+    public void adding(Car car) {
+    }
+
+    //removing index
+    public void remove(Car car) {
+    }
+
+    //show indexes
+    public void show() {
+        System.out.println(cars.length);
+    }
+}
--- src/DataStructure/Array/Main.java
+++ src/DataStructure/Array/Main.java
@@ -2,6 +2,13 @@ package DataStructure.Array;
 
 public class Main {
     public static void main(String[] args) {
+        var car1 = new Car("BMW");
+        var car2 = new Car("Ferrai");
 
+        var list = new Array();
+
+       list.adding(car1);
+       list.adding(car2);
+       list.show();
     }
 }
