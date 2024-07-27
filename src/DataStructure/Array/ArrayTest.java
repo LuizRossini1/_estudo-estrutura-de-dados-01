d58d999fac8cadf7df8460f1f8eb4e559cba64c0
LuizRossini <luiz.guirossini123@gmail.com>
2024-07-26 22:23:22 -0300

    creating and adding on list new object

--- src/DataStructure/Array/ArrayTest.java
+++ src/DataStructure/Array/ArrayTest.java
@@ -5,12 +5,14 @@ public class ArrayTest {
         var car1 = new Car("BMW");
         var car2 = new Car("Ferrai");
         var car3 = new Car("Troller");
+        var car4 = new Car("Tesla");
 
         var list = new Array();
 
         list.adding(car1);
         list.adding(car2);
         list.adding(car3);
+        list.adding(car4);
         list.remove(1);
 
         System.out.println(list.have(car3));
